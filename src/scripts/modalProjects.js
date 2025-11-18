f14369b5ac61bfd96136f7a5c6c6ca56ab6b0391
Raphael <raphaelhsmissias@gmail.com>
2025-11-18 10:05:33 -0300

    feat: add modal html items for each projects

---
+++ src/scripts/modalProjects.js
@@ -0,0 +1,154 @@
+const raruEarnings = () => {
+    return `
+        <h2 id="modalTitle">RaruEarnings</h2>
+        <div class="modal_item w-full justify-center flex gap-4 rounded-md mb-4">
+            <img class="modal_img max-h-[350px] max-w-[400px] max-sm:h-full max-sm:w-full rounded-md" src="./src/assets/img/raruEarnings/menu.jpg" alt="project_photo">
+            <div class="modal_info h-full w-full gap-2 rounded-md">
+                <h3>Menu</h3>
+                <p>O menu é composto por 3 seções: Ordens, Clientes e Trabalhos.</p>
+                <img class="h-7" src="./src/assets/icons/hover.png" alt="hover_icon">
+            </div>
+        </div>
+        <div class="modal_item w-full justify-center flex gap-4 rounded-md mb-4">
+            <img class="modal_img max-h-[350px] max-w-[400px] max-sm:h-full max-sm:w-full rounded-md" src="./src/assets/img/raruEarnings/clients.jpg" alt="project_photo">
+            <div class="modal_info h-full w-full gap-2 rounded-md">
+                <h3>Clientes</h3>
+                <p>A área de cadastro de clientes contém interface limpa e uma tabela de fácil visualização, permitindo atualizar e excluir informações de maneira rápida e simples.</p>
+                <img class="h-7" src="./src/assets/icons/hover.png" alt="hover_icon">
+            </div>
+        </div>
+        <div class="modal_item max-sm:p-12 w-full justify-center items-center flex gap-4 rounded-md mb-4">
+            <img class="modal_img max-h-[350px] max-w-[400px] max-sm:h-full max-sm:w-full rounded-md" src="./src/assets/img/raruEarnings/transactions.jpg" alt="project_photo">
+            <div class="modal_info h-full w-full gap-2 rounded-md">
+                <h3>Valores</h3>
+                <p>Visão geral que levará em conta o total de valores por trabalho e calculará possíveis divergências no salário recebido.</p>
+                <img class="h-7" src="./src/assets/icons/hover.png" alt="hover_icon">
+            </div>
+        </div>
+        <div class="modal_item w-full justify-center flex gap-4 rounded-md mb-4">
+            <img class="modal_img max-h-[350px] max-w-[400px] max-sm:h-full max-sm:w-full rounded-md" src="./src/assets/img/raruEarnings/orders.jpg" alt="project_photo">
+            <div class="modal_info h-full w-full gap-2 rounded-md">
+                <h3>Ordens</h3>
+                <p>Cartões clicáveis que permitem uma visão ampla e controle das ordens já criadas.</p>
+                <img class="h-7" src="./src/assets/icons/hover.png" alt="hover_icon">
+            </div>
+        </div>
+        <div class="modal_item w-full justify-center flex gap-4 rounded-md mb-4">
+            <img class="modal_img max-h-[350px] max-w-[400px] max-sm:h-full max-sm:w-full rounded-md" src="./src/assets/img/raruEarnings/order.jpg" alt="project_photo">
+            <div class="modal_info h-full w-full gap-2 rounded-md">
+                <h3>Ordem</h3>
+                <p>Alguns campos possuem informações pré definidas pelo banco de dados, permitindo uma adição fácil e otimizada.</p>
+                <img class="h-7" src="./src/assets/icons/hover.png" alt="hover_icon">
+            </div>
+        </div>
+        <div class="modal_item w-full justify-center flex gap-4 rounded-md mb-4">
+            <img class="modal_img max-h-[350px] max-w-[400px] max-sm:h-full max-sm:w-full rounded-md" src="./src/assets/img/raruEarnings/history.jpg" alt="project_photo">
+            <div class="modal_info h-full w-full gap-2 rounded-md">
+                <h3>Histórico</h3>
+                <p>Histórico dos valores recebidos ou descontos da empresa.</p>
+                <img class="h-7" src="./src/assets/icons/hover.png" alt="hover_icon">
+            </div>
+        </div>
+    `
+}
+
+const tlsvProject = () => {
+    return `
+        <h2 id="modalTitle">TLSV_Project</h2>
+        <div class="modal_item w-full justify-center flex gap-4 rounded-md mb-4">
+            <img class="modal_img max-h-[350px] max-w-[400px] max-sm:h-full max-sm:w-full rounded-md" src="./src/assets/img/pychse_square.jpg" alt="project_photo">
+            <div class="modal_info h-full w-full gap-2 rounded-md">
+                <h3>Uso</h3>
+                <p>Basta salvar a planilha gerada pelo aplicativo da Oracle em uma das pastas e executar para que o programa faça a leitura e retorne o arquivo com as informações desejadas.</p>
+                <img class="h-7" src="./src/assets/icons/hover.png" alt="hover_icon">
+            </div>
+        </div>
+        <div class="modal_item w-full justify-center flex gap-4 rounded-md mb-4">
+            <img class="modal_img modal_img max-h-[350px] max-w-[400px] max-sm:h-full max-sm:w-full rounded-md" src="./src/assets/img/oracle.png" alt="project_photo">
+            <div class="modal_info h-full w-full gap-2 rounded-md">
+                <h3>Funcionamento</h3>
+                <p>A partir da planilha, o programa seleciona os contratos e faz buscas no aplicativo da Oracle, retornando as informações dos campos definidos em código.</p>
+                <p>(Imagem ilustrativa)</p>
+                <img class="h-7" src="./src/assets/icons/hover.png" alt="hover_icon">
+            </div>
+        </div>
+
+    `
+}
+
+const raruPharmacies = () => {
+    return `
+        <h2 id="modalTitle">RaruPharmacies</h2>
+        <div class="modal_item w-full justify-center flex gap-4 rounded-md mb-4">
+            <img class="modal_img modal_img max-h-[350px] max-w-[400px] max-sm:h-full max-sm:w-full rounded-md" src="./src/assets/img/raruPharmacies/login.jpg" alt="project_photo">
+            <div class="modal_info h-full w-full gap-2 rounded-md">
+                <h3>Login</h3>
+                <p>Tela inicial onde somente pessoas autorizadas e de suas respectivas áreas poderão ter acesso.</p>
+                <img class="h-7" src="./src/assets/icons/hover.png" alt="hover_icon">
+            </div>
+        </div>
+        <div class="modal_item w-full justify-center flex gap-4 rounded-md mb-4">
+            <img class="modal_img max-h-[350px] max-w-[400px] max-sm:h-full max-sm:w-full rounded-md" src="./src/assets/img/raruPharmacies/main.jpg" alt="project_photo">
+            <div class="modal_info h-full w-full gap-2 rounded-md">
+                <h3>Tela principal</h3>
+                <p>O programa conta com um sistema de níveis, onde cada funcionário tem acesso somente à sua área pertencente, as demais funções estarão bloqueadas.</p>
+                <img class="h-7" src="./src/assets/icons/hover.png" alt="hover_icon">
+            </div>
+        </div>
+        <div class="modal_item w-full justify-center flex gap-4 rounded-md mb-4">
+            <img class="modal_img max-h-[350px] max-w-[400px] max-sm:h-full max-sm:w-full rounded-md" src="./src/assets/img/raruPharmacies/products.jpg" alt="project_photo">
+            <div class="modal_info h-full w-full gap-2 rounded-md">
+                <h3>Produtos e estoque</h3>
+                <p>Permite o cadastro de produtos e a visualização de produtos com baixo estoque, mantendo maior controle das quantidades.</p>
+                <img class="h-7" src="./src/assets/icons/hover.png" alt="hover_icon">
+            </div>
+        </div>
+        <div class="modal_item w-full justify-center flex gap-4 rounded-md mb-4">
+            <img class="modal_img max-h-[350px] max-w-[400px] max-sm:h-full max-sm:w-full rounded-md" src="./src/assets/img/raruPharmacies/users.jpg" alt="project_photo">
+            <div class="modal_info h-full w-full gap-2 rounded-md">
+                <h3>Usuários</h3>
+                <p>O cadastro de usuários conta com senhas criptografadas e sistema de níveis para bloqueio de acesso a determinadas áreas.</p>
+                <img class="h-7" src="./src/assets/icons/hover.png" alt="hover_icon">
+            </div>
+        </div>
+        <div class="modal_item w-full justify-center flex gap-4 rounded-md mb-4">
+            <img class="modal_img max-h-[350px] max-w-[400px] max-sm:h-full max-sm:w-full rounded-md" src="./src/assets/img/raruPharmacies/sales.jpg" alt="project_photo">
+            <div class="modal_info h-full w-full gap-2 rounded-md">
+                <h3>Vendas</h3>
+                <p>Visualização detalhada dos itens para compra.</p>
+                <img class="h-7" src="./src/assets/icons/hover.png" alt="hover_icon">
+            </div>
+        </div>
+        <div class="modal_item w-full justify-center flex gap-4 rounded-md mb-4">
+            <img class="modal_img max-h-[350px] max-w-[400px] max-sm:h-full max-sm:w-full rounded-md" src="./src/assets/img/raruPharmacies/finishing_sale.jpg" alt="project_photo">
+            <div class="modal_info h-full w-full gap-2 rounded-md">
+                <h3>Finalizando a venda</h3>
+                <p>Controle dos valores.</p>
+                <img class="h-7" src="./src/assets/icons/hover.png" alt="hover_icon">
+            </div>
+        </div>
+    `
+}
+
+const raruBooks = () => {
+    return `
+        <h2 id="modalTitle">RaruBooks</h2>
+        <div class="modal_item w-full justify-center flex gap-4 rounded-md mb-4">
+            <img class="modal_img modal_img max-h-[350px] max-w-[400px] max-sm:h-full max-sm:w-full rounded-md" src="./src/assets/img/raruBooks.jpg" alt="project_photo">
+            <div class="modal_info h-full w-full gap-2 rounded-md">
+                <h3>API</h3>
+                <p>Feita para controle de biblioteca, contém sistema de usuários e emprétimo por número de cópias.</p>
+                <img class="h-7" src="./src/assets/icons/hover.png" alt="hover_icon">
+            </div>
+        </div>
+    `
+}
+
+const modalProjects = {
+    raruEarnings: raruEarnings(),
+    tlsvProject: tlsvProject(),
+    raruPharmacies: raruPharmacies(),
+    raruBooks: raruBooks()
+};
+
+export default modalProjects;
