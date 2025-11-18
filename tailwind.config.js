d7c2608aa7b66b37f08678527707b62b3d55a9eb
Raphael <raphaelhsmissias@gmail.com>
2025-11-18 16:28:09 -0300

    feat: complete implementation of general draft

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#080808',
+        secondary: '#3F8E00',
+        tertiary: '#1B1B1B',
+      },
+      backgroundColor: {
+        primary: '#222',
+        secondary: '#333',
+        tertiary: '#444',
+      },
+      textColor: {
+        primary: '#fff',
+        secondary: '#f5f5f5',
+        tertiary: '#ddd',
+      },
+    },
+  },
+}
